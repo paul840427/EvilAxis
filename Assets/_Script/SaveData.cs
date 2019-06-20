4f3914e37f87f46bfa1e7a9f4e65e3afa1320cef
j32u4ukh <j32u4ukh@gmail>
2019-06-20 10:42:57 +0800

    test header version to download image

--- Assets/_Script/SaveData.cs
+++ Assets/_Script/SaveData.cs
@@ -12,6 +12,8 @@ public class SaveData : MonoBehaviour
     string guid;
     string get_request;
 
+    WWWForm form;
+
     private void Start()
     {
         server = "http://140.122.91.200";
@@ -23,18 +25,14 @@ public class SaveData : MonoBehaviour
     {
         if(GameInfo.Version == EVersion.Test)
         {
-            if (Input.GetKeyDown(KeyCode.U))
+            if (Input.GetKeyDown(KeyCode.S))
             {
                 StartCoroutine(screenShot());
             }
-
-            //if (Input.GetKeyDown(KeyCode.D))
-            //{
-            //    StartCoroutine(deleteDir());
-            //}
         }
     }
 
+    #region local version
     public void saveClickProcess(Vector3 pos, EFunction type)
     {
         // 檢查資料夾是否存在，不存在則建立
@@ -65,30 +63,42 @@ public class SaveData : MonoBehaviour
         writer.Close();
         writer.Dispose();
     }
+    #endregion
 
-    #region upload screen shot
+    #region webGL version
     public IEnumerator screenShot()
     {
+        print("start screenShot");
         yield return new WaitForEndOfFrame();
+        // Capture Screenshot
         Texture2D texture = ScreenCapture.CaptureScreenshotAsTexture();
         // texture to bytes
         byte[] bytes = texture.EncodeToPNG();
 
-        url = string.Format("{0}/{1}", server, "ScreenShot.php");
+        // upload image
+        yield return StartCoroutine(uploadImage(bytes));
 
-        WWWForm form = new WWWForm();
+        // wait for get_request
+        yield return StartCoroutine(getFilesName());
+
+        // download image
+        yield return StartCoroutine(downloadImage());
+
+        // delete dir in server
+        yield return StartCoroutine(deleteDir());
+
+        print("end screenShot");
+    }
+
+    IEnumerator uploadImage(byte[] bytes)
+    {
+        url = string.Format("{0}/{1}", server, "ScreenShot.php");
+        form = new WWWForm();
         form.AddField("guid", guid);
         string time = DateTime.Now.ToString("yyyy-MM-dd@H-mm-ss-ffff");
         form.AddField("time", time);
         form.AddBinaryData("file", bytes);
 
-        yield return StartCoroutine(Upload(url, form));
-
-        StartCoroutine(deleteDir());
-    }
-
-    IEnumerator Upload(string url, WWWForm form)
-    {
         using (UnityWebRequest www = UnityWebRequest.Post(url, form))
         {
             yield return www.SendWebRequest();
@@ -105,51 +115,14 @@ public class SaveData : MonoBehaviour
             }
         }
     }
-    #endregion
 
-    #region download image
-    public IEnumerator DownloadFile()
+    IEnumerator getFilesName()
     {
         print("start GetFilesName");
         url = string.Format("{0}/{1}", server, "GetFilesName.php"); ;
-        WWWForm form = new WWWForm();
+        form = new WWWForm();
         form.AddField("guid", guid);
 
-        // wait for get_request
-        yield return StartCoroutine(getFilesName(url, form));
-        print("end GetFilesName");
-
-        UnityWebRequest uwr;
-        string dir = string.Format("{0}/image/{1}", server, guid);
-        string[] files_name = get_request.Split('#');
-        int i, len = files_name.Length - 1;
-        string file_path;
-
-        for (i = 0; i < len; i++)
-        {
-            print("file_name: " + files_name[i]);
-            url = string.Format("{0}/{1}", dir, files_name[i]);
-            uwr = new UnityWebRequest(url, UnityWebRequest.kHttpVerbGET);
-
-
-            file_path = Path.Combine(GameInfo.DataPath, files_name[i]);
-            uwr.downloadHandler = new DownloadHandlerFile(file_path);
-            yield return uwr.SendWebRequest();
-
-            if (uwr.isNetworkError || uwr.isHttpError)
-            {
-                print(uwr.error);
-            }
-            else
-            {
-                print("File successfully downloaded and saved to " + file_path);
-            }
-                
-        }
-    }
-
-    IEnumerator getFilesName(string url, WWWForm form)
-    {
         using (UnityWebRequest www = UnityWebRequest.Post(url, form))
         {
             yield return www.SendWebRequest();
@@ -165,16 +138,80 @@ public class SaveData : MonoBehaviour
                 print(get_request);
             }
         }
+
+        print("end GetFilesName");
+    }
+
+    //public IEnumerator DownloadFile()
+    //{
+    //    UnityWebRequest uwr;
+    //    string dir = string.Format("{0}/image/{1}", server, guid);
+    //    string[] files_name = get_request.Split('#');
+    //    int i, len = files_name.Length - 1;
+    //    string file_path;
+
+    //    for (i = 0; i < len; i++)
+    //    {
+    //        print("file_name: " + files_name[i]);
+    //        url = string.Format("{0}/{1}", dir, files_name[i]);
+    //        uwr = new UnityWebRequest(url, UnityWebRequest.kHttpVerbGET);
+
+
+    //        file_path = Path.Combine(GameInfo.DataPath, files_name[i]);
+    //        uwr.downloadHandler = new DownloadHandlerFile(file_path);
+    //        yield return uwr.SendWebRequest();
+
+    //        if (uwr.isNetworkError || uwr.isHttpError)
+    //        {
+    //            print(uwr.error);
+    //        }
+    //        else
+    //        {
+    //            print("File successfully downloaded and saved to " + file_path);
+    //        }
+                
+    //    }
+    //}
+
+    IEnumerator downloadImage()
+    {
+        print("start downloadImage");
+        url = string.Format("{0}/{1}", server, "DownloadImage2.php"); ;
+
+        string[] files_name = get_request.Split('#');
+        int i, len = files_name.Length - 1;
+        string file_name;
+
+        for (i = 0; i < len; i++) {
+            file_name = files_name[i];
+
+            form = new WWWForm();
+            form.AddField("guid", guid);
+            form.AddField("file_name", file_name);
+
+            using (UnityWebRequest www = UnityWebRequest.Post(url, form))
+            {
+                yield return www.SendWebRequest();
+
+                if (www.isNetworkError || www.isHttpError)
+                {
+                    print(www.error);
+                }
+                else
+                {
+                    print(string.Format("download {0} success.", file_name));
+                }
+            }
+        }
+
+        print("end downloadImage");
     }
 
     IEnumerator deleteDir()
     {
-        yield return StartCoroutine(DownloadFile());
-
-        WWWForm form = new WWWForm();
-        form.AddField("guid", guid);
-
         url = string.Format("{0}/{1}", server, "DeleteDir.php");
+        form = new WWWForm();
+        form.AddField("guid", guid);
 
         using (UnityWebRequest www = UnityWebRequest.Post(url, form))
         {
